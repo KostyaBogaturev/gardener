a1ef5e8f17991f63a6d741260e4aa2d4a930bc03
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-11 01:25:34 +0200

    Added new solution "Gardener"

---
+++ Gardener/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Gardener
+{
+    using System;
+
+    /// <summary>
+    /// Class with method Main.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Start point of program.
+        /// </summary>
+        /// <param name="args">standart argument.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
