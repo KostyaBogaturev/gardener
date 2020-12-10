3aec5de150652910fe99cf2df5aa03c010a35a67
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-11 01:46:38 +0200

    Added interface IOrganism

---
+++ Gardener/Abstractions/IOrganism.cs
@@ -0,0 +1,23 @@
+﻿namespace Gardener.Abstractions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Gardener.Enums;
+
+    /// <summary>
+    /// Interface of live organism.
+    /// </summary>
+    public interface IOrganism
+    {
+        /// <summary>
+        /// Gets average size of organism.
+        /// </summary>
+        public double AverageSize { get; }
+
+        /// <summary>
+        /// Gets kingdom of organism.
+        /// </summary>
+        public Kingdoms Kingdom { get; }
+    }
+}
