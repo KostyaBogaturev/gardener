e11df26b502711f0d89c5fc1d1725adccdabf775
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-11 01:32:20 +0200

    Added enum of kingdoms nature

---
+++ Gardener/Enums/Kingdoms.cs
@@ -0,0 +1,28 @@
+﻿namespace Gardener.Enums
+{
+    /// <summary>
+    /// Enum of kingdoms of organism.
+    /// </summary>
+    public enum Kingdoms
+    {
+        /// <summary>
+        /// Plant.
+        /// </summary>
+        Plants,
+
+        /// <summary>
+        /// Animal.
+        /// </summary>
+        Animals,
+
+        /// <summary>
+        /// Mushrooms.
+        /// </summary>
+        Mushrooms,
+
+        /// <summary>
+        /// Viruses.
+        /// </summary>
+        Viruses,
+    }
+}
