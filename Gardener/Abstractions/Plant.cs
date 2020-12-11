6dacca8b9e4c303b33bafe6fabb5fc2821378888
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-11 02:35:19 +0200

    Added enum of planr's devision

--- Gardener/Abstractions/Plant.cs
+++ Gardener/Abstractions/Plant.cs
@@ -12,16 +12,19 @@
     {
         private readonly double averageSize;
         private readonly Prevalence rarity;
+        private readonly PlantDevision devision;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Plant"/> class.
         /// </summary>
         /// <param name="averageSize">initialize avarege size.</param>
         /// <param name="rarity">initialize prevalence.</param>
-        public Plant(double averageSize, Prevalence rarity)
+        /// <param name="devision">initialize devision.</param>
+        public Plant(double averageSize, Prevalence rarity, PlantDevision devision)
         {
             this.averageSize = averageSize;
             this.rarity = rarity;
+            this.devision = devision;
         }
 
         /// <summary>
