eb922d4ac31162048e68db250d242082f7d05abb
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-11 03:31:16 +0200

    Added LowPlant.cs

---
+++ Gardener/Abstractions/LowPlant.cs
@@ -0,0 +1,32 @@
+﻿namespace Gardener.Abstractions
+{
+    using System;
+    using Gardener.Enums;
+
+    /// <summary>
+    /// Abstract class wich discript low devision plants.
+    /// </summary>
+    public abstract class LowPlant : Plant
+    {
+        private const string Info = "Higher plants belong to the department Streptophyta and its subgroup Streptophytina. All of them are multicellular nuclear organisms with specialized reproductive organs.";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LowPlant"/> class.
+        /// </summary>
+        /// <param name="averageSize">initialize average size.</param>
+        /// <param name="rarity">initialize prevalence.</param>
+        public LowPlant(double averageSize, Prevalence rarity)
+            : base(averageSize, rarity, PlantDevision.HigherPlants)
+        {
+        }
+
+        /// <summary>
+        /// Override method GetInfo , add new information about high devision plants.
+        /// </summary>
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine(Info);
+        }
+    }
+}
