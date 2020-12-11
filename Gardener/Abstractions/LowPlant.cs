6ae665baff5ba05df07d74c3e7a2c17b4db43c0a
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-11 03:41:36 +0200

    Added enum of classes lower devision plants

--- Gardener/Abstractions/LowPlant.cs
+++ Gardener/Abstractions/LowPlant.cs
@@ -8,16 +8,19 @@
     /// </summary>
     public abstract class LowPlant : Plant
     {
-        private const string Info = "Higher plants belong to the department Streptophyta and its subgroup Streptophytina. All of them are multicellular nuclear organisms with specialized reproductive organs.";
+        private const string Info = "Organisms that do not have morphological division of the body into vegetative organs";
+        private readonly LowerPlantsClasses lpclass;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="LowPlant"/> class.
         /// </summary>
         /// <param name="averageSize">initialize average size.</param>
         /// <param name="rarity">initialize prevalence.</param>
-        public LowPlant(double averageSize, Prevalence rarity)
-            : base(averageSize, rarity, PlantDevision.HigherPlants)
+        /// <param name="lpclass">initialize class.</param>
+        public LowPlant(double averageSize, Prevalence rarity, LowerPlantsClasses lpclass)
+            : base(averageSize, rarity, PlantDevision.LowerPlants)
         {
+            this.lpclass = lpclass;
         }
 
         /// <summary>
