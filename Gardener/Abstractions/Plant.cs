ac6405c2b6e5f80818a0ac4c74572571ebc2584b
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-11 02:22:09 +0200

    Added abstruct class plant

---
+++ Gardener/Abstractions/Plant.cs
@@ -0,0 +1,49 @@
+﻿namespace Gardener.Abstractions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Gardener.Enums;
+
+    /// <summary>
+    /// Abstruct class plant.
+    /// </summary>
+    public abstract class Plant : IOrganism
+    {
+        private readonly double averageSize;
+        private readonly Prevalence rarity;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Plant"/> class.
+        /// </summary>
+        /// <param name="averageSize">initialize avarege size.</param>
+        /// <param name="rarity">initialize prevalence.</param>
+        public Plant(double averageSize, Prevalence rarity)
+        {
+            this.averageSize = averageSize;
+            this.rarity = rarity;
+        }
+
+        /// <summary>
+        /// Gets implement interface property AverageSize.
+        /// </summary>
+        public double AverageSize => this.averageSize;
+
+        /// <summary>
+        /// Gets implement interface property Kingdom
+        /// For this class Kingdom = Plants.
+        /// </summary>
+        public Kingdoms Kingdom => Kingdoms.Plants;
+
+        /// <summary>
+        /// Gets implement interface property Rarity.
+        /// </summary>
+        public Prevalence Rarity => this.rarity;
+
+        /// <summary>
+        /// Gets implement interface property WayGettingEnergy
+        /// For this class WayGettingEnergy = Photosynthesis.
+        /// </summary>
+        public GettingEnergy WayGettingEnergy => GettingEnergy.Photosynthesis;
+    }
+}
