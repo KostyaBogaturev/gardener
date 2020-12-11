f6cb623c1c29f1e206ee90d001c39b7058b59e6a
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-11 14:31:40 +0200

    Added class garden (collection of our plants)

---
+++ Gardener/Models/Garden.cs
@@ -0,0 +1,32 @@
+﻿namespace Gardener.Models
+{
+    using System;
+    using Gardener.Abstractions;
+
+    /// <summary>
+    /// In class garden we will keep our plants.
+    /// </summary>
+    public class Garden
+    {
+        private Plant[] garden;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Garden"/> class.
+        /// </summary>
+        public Garden()
+        {
+            this.garden = new Plant[1];
+        }
+
+        /// <summary>
+        /// Add new item to array garden.
+        /// </summary>
+        /// <param name="plant">new item.</param>
+        public void Add(Plant plant)
+        {
+            int lenth = this.garden.Length;
+            this.garden[lenth - 1] = plant;
+            Array.Resize(ref this.garden, lenth + 1);
+        }
+    }
+}
