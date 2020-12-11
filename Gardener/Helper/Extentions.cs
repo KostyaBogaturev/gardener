9b1d4c3dbc12f94c5b8bb81cd1e6862cf6382b08
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-11 14:43:50 +0200

    Added helper with extention

---
+++ Gardener/Helper/Extentions.cs
@@ -0,0 +1,59 @@
+﻿namespace Gardener.Helper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Gardener.Abstractions;
+    using Gardener.Enums;
+
+    /// <summary>
+    /// Extention for array of plants.
+    /// </summary>
+    public static class Extentions
+    {
+        /// <summary>
+        /// Extention for find item in plant arr.
+        /// </summary>
+        /// <param name="array">array name.</param>
+        /// <param name="rarity">rarity of plant.</param>
+        /// <returns>Finded plant or null.</returns>
+        public static Plant Find(this Plant[] array, Prevalence rarity)
+        {
+            Plant toReturn = null;
+
+            foreach (Plant item in array)
+            {
+                if (item.Rarity == rarity)
+                {
+                    toReturn = item;
+                    break;
+                }
+            }
+
+            return toReturn;
+        }
+
+        /// <summary>
+        /// Extention for find item in plant arr.
+        /// </summary>
+        /// <param name="array">array name.</param>
+        /// <param name="rarity">rarity of item.</param>
+        /// <param name="averageSize">average size of plant.</param>
+        /// <returns>Finded plant or null.</returns>
+        public static Plant Find(this Plant[] array, Prevalence rarity, double averageSize)
+        {
+            Plant toReturn = null;
+
+            foreach (Plant item in array)
+            {
+                if (item.Rarity == rarity && item.AverageSize == averageSize)
+                {
+                    toReturn = item;
+                    break;
+                }
+            }
+
+            return toReturn;
+        }
+    }
+}
